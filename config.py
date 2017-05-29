5c749b7034352206fb4b5d355d3c105f62f1c2e3
JasonwLi <z467li@edu.uwaterloo.ca>
2017-05-29 00:20:08 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-username = "OnlyJustOnce"
-password = "960918"
-client_id = "giFsOGexju_Ttg"
-client_secret = "NHHRCqVWe_uvOlQ0TwnQX7Aqw14"
+username = "username"
+password = "password"
+client_id = "key"
+client_secret = "key"
